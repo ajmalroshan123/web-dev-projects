6d5ad0a0a06da4ae2e6109f87a549153b55578aa
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-09-30 19:28:20 +0530

    making the paddle move

--- web_dev_projects/pippong-game/index.js
+++ web_dev_projects/pippong-game/index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    let table = document.getElementById("ping-pong-table");
     let ball = document.getElementById("ball");
+    let paddle = document.getElementById("paddle")
 
     let posX = 50; // Initial horizontal position
     let posY = 50; // Initial vertical position
@@ -14,12 +16,36 @@ document.addEventListener("DOMContentLoaded", () => {
     setInterval( function exec() {
 
         posX += velocityX;
+        posY += velocityY;
 
         ball.style.left = `${posX}px`;
+        ball.style.top = `${posY}px`;
 
-        if (posX > 700 || posX <= 0) {
+        if (posX > table.offsetWidth - ball.offsetWidth || posX <= 0) {
             velocityX = -velocityX;
         }
-    }, 10);
+
+        if (posY > table.offsetHeight - ball.offsetHeight || posY <= 0) {
+            velocityY = -velocityY;
+        }
+    }, 7);
+
+
+    // paddle movement
+
+    let paddlePosY = 0;
+    let dPy = 5;
+
+    document.addEventListener("keydown", (event) => {
+        if (event.keyCode == 38 && paddlePosY > 0) {
+            paddlePosY += (-1) * dPy;
+        }
+        else if (event.keyCode == 40 && paddlePosY < table.offsetHeight - paddle.offsetHeight)
+        {
+            paddlePosY += dPy;
+        }
+
+        paddle.style.top = `${paddlePosY}px`;
+    })
 
 })
